e287c9b65572e6d0c65a76db4701b1b15906c657
AdelyJfer <adely.jfer@gmail.com>
2020-10-12 11:51:28 -0400

    homework 8

--- exercises/08-currying-and-callbacks/currying-and-callbacks.js
+++ exercises/08-currying-and-callbacks/currying-and-callbacks.js
@@ -13,7 +13,17 @@
  * @param {number} a
  * @param {number} b
  */
-const multiplier = (a, b) => {};
+const multiplier = (a = null, b = null) => {
+ if (a != null && b != null) {
+   return a * b;
+ } else if (b == null) {
+   return function fn(other) {
+     return multiplier(a, other);
+   };
+ }
+
+
+};
 
 /**
  * Create a function called "printer".
@@ -25,6 +35,10 @@ const multiplier = (a, b) => {};
  *  - Matina
  * @param {string} name instructor name
  */
+
+const printer = (name) => {
+  console.log("- ", name);
+};
 
 /**
  * NOTE that the test will use the "printer" function that you completed in the problem above.
@@ -38,7 +52,14 @@ const multiplier = (a, b) => {};
  * // - Jamal
  * // - Matina
  */
-const printNames = (array, callback) => {};
+const printNames = (array, callback) => {
+
+array.map((name) => {
+  callback(name);
+});
+
+
+};
 
 /**
  * Ignore everything below this line. It is for the tests.
