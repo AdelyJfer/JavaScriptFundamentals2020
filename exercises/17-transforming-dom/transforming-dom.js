f3a0f44435ebcebb8fb35f74a35f27018f7150c5
AdelyJfer <adely.jfer@gmail.com>
2020-10-26 14:18:35 -0400

    hw17

--- exercises/17-transforming-dom/transforming-dom.js
+++ exercises/17-transforming-dom/transforming-dom.js
@@ -3,3 +3,33 @@
  * to transform various elements on the page in different ways.
  * You may modify the HTML to add ids, classes, data attributes, etc.
  */
+
+let danger = document.querySelector("[data-danger]");
+danger.setAttribute("rol", "alert");
+let link = document.querySelector("[linkjs]");
+link.setAttribute(
+  "href",
+  "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+);
+let changeText = document.querySelector("[data-success]");
+changeText.textContent = "I am victorious!";
+
+let backColor = document.querySelector("[data-info]");
+backColor.setAttribute("style", "background-color: red");
+
+let textColor = document.querySelector("[changeColor]");
+let currentClass = textColor.getAttribute("class");
+currentClass = currentClass + " text-dark";
+textColor.setAttribute("class", currentClass);
+
+let hideDiv = document.querySelector("[data-hide]");
+hideDiv.setAttribute("style", "visibility: hidden");
+
+let alertHide = document.querySelector("[alert-hide]");
+alertHide.setAttribute("class", "alert alert-success");
+
+let codeDiv = document.querySelector("[code]");
+let button = document.querySelector("#idButton");
+if (button.getAttribute("class").includes("btn-primary")) {
+  codeDiv.textContent = "✓ blue";
+}
